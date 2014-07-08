e5bb5bf5500d039dda9eedf609dd7b1746d1a6eb
Markas Korotkovas <mkorotkovas@yahoo.com>
2014-07-08 13:48:44 -0600

    added config

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,3 +1,6 @@
+var initDone = false;
+var tempScale = 'F';
+
 function sendToWatchSuccess(e)
 {
   console.log("Message sent to watch successfully!");
@@ -32,8 +35,15 @@ function fetchWeather(latitude, longitude) {
             && !isNaN(response.main.temp)) {
           temperatureC = Math.round(response.main.temp - 273.15);
           temperatureF = Math.round(temperatureC * 9 / 5 + 32);
-          //TODO assign temp based on settings
-          temperature = temperatureF;
+          //assign temp based on settings
+          if (tempScale == 'C')
+          {
+            temperature = temperatureC;
+          }
+          else
+          {
+            temperature = temperatureF;
+          }
         }
         if (response && response.name ) {
           location = response.name;
@@ -71,14 +81,41 @@ function locationError(err) {
   });
 }
 
+function initConfigOptions()
+{
+  var tempScaleLS = localStorage.getItem('tempScale');
+  if (tempScaleLS != null && tempScaleLS != 'undefined' && tempScaleLS.length == 1)
+  {
+    tempScale = tempScaleLS;
+    console.log("Assigned tempScale from storage=" + tempScaleLS);
+  }
+}
+
+function storeConfigOptions(inOptions)
+{
+  if (inOptions != null && inOptions != 'undefined')
+  {
+    if (inOptions.tempScale != null && inOptions.tempScale.length == 1)
+    {
+      localStorage.setItem('tempScale', inOptions.tempScale);
+      tempScale = inOptions.tempScale;
+    }
+  }
+}
+
 var locationOptions = { "timeout": 15000, "maximumAge": 600000 };//15s, 10 minutes 
 
 
 Pebble.addEventListener("ready",
                         function(e) {
-                          console.log("connect!" + e.ready);
-                          locationWatcher = window.navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
-                          console.log(e.type);
+                          console.log("JS - ready called" + e.ready);
+                          if (!initDone)
+                          {
+                            console.log("JS - performing init tasks" + e.ready);
+                            locationWatcher = window.navigator.geolocation.watchPosition(locationSuccess, locationError, locationOptions);
+                            initConfigOptions();
+                            initDone = true;
+                          }
                         });
 
 Pebble.addEventListener("appmessage",
@@ -94,14 +131,15 @@ Pebble.addEventListener("webviewclosed",
                          console.log("webview closed");
                          var options = JSON.parse(decodeURIComponent(e.response));
                          console.log("Options = " + JSON.stringify(options));
-                         console.log(e.type);
-                         console.log(e.response);
+                         storeConfigOptions(options);
+                         window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
                          });
 
 Pebble.addEventListener("showConfiguration", 
                          function() {
                          console.log("showing configuration");
-                         Pebble.openURL('http://pebbleappcfg.herokuapp.com/GeekyTime/geekyTimeCfg.html');
+                         initConfigOptions();
+                         Pebble.openURL('http://pebbleappcfg.herokuapp.com/GeekyTime/geekyTimeCfg.html?tempScale=' + tempScale);
                          });
 
 
