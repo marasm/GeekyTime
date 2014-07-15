0e2149953de307422698a00f985b2bf6c36b7ef7
Markas Korotkovas <mkorotkovas@yahoo.com>
2014-07-14 22:11:44 -0600

    added comm icon

--- src/geeky_time.c
+++ src/geeky_time.c
@@ -10,10 +10,12 @@ static TextLayer *temp_layer;
 static TextLayer *weather_loc_layer;
 
 static BitmapLayer *bt_layer;
+static BitmapLayer *comm_layer;
 static BitmapLayer *battery_layer;
 static BitmapLayer *icon_layer;
 static BitmapLayer *therm_layer;
 static GBitmap *bt_bitmap = NULL;
+static GBitmap *comm_bitmap = NULL;
 static GBitmap *battery_bitmap = NULL;
 static GBitmap *icon_bitmap = NULL;
 static GBitmap *therm_bitmap = NULL;
@@ -69,14 +71,14 @@ static bool is_valid_temp(const char * st)
 
 static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
-  // if (icon_bitmap) 
-  // {
-  //   gbitmap_destroy(icon_bitmap);
-  // }
-  // icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMG_WEATHER_00);
-  // bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
-  // text_layer_set_text(temp_layer, "--");
-  // text_layer_set_text(weather_loc_layer, "Unknown");
+  if (comm_bitmap)
+  {
+    gbitmap_destroy(comm_bitmap);
+  }
+  comm_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMG_COMM_ERR);
+  bitmap_layer_set_bitmap(comm_layer, comm_bitmap);
+  layer_mark_dirty(bitmap_layer_get_layer(comm_layer));
+  
 }
 
 static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
@@ -189,12 +191,29 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
 
     case WEATHER_LOCATION_KEY:
       text_layer_set_text(weather_loc_layer, new_tuple->value->cstring);
+
+      //update the comm icon only once per call
+      if (comm_bitmap)
+      {
+        gbitmap_destroy(comm_bitmap);
+      }
+      comm_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMG_COMM_OFF);
+      bitmap_layer_set_bitmap(comm_layer, comm_bitmap);
+      layer_mark_dirty(bitmap_layer_get_layer(comm_layer));
       break;
   }
 }
 
 static void send_cmd(void) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Sending sync message to phone...");
+  if (comm_bitmap)
+  {
+    gbitmap_destroy(comm_bitmap);
+  }
+  comm_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMG_COMM_ON);
+  bitmap_layer_set_bitmap(comm_layer, comm_bitmap);
+  layer_mark_dirty(bitmap_layer_get_layer(comm_layer));
+
   Tuplet value = TupletInteger(1, 1);
 
   DictionaryIterator *iter;
@@ -212,9 +231,22 @@ static void send_cmd(void) {
 
 static void handle_tap(AccelAxisType axis, int32_t direction)
 {
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "tap axis=%i", axis);
   APP_LOG(APP_LOG_LEVEL_DEBUG, "tap direction=%i", (int)direction);
-  send_cmd();
+  switch (axis) {
+    case ACCEL_AXIS_X:
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "tap axis=X");
+      break;
+    case ACCEL_AXIS_Y:
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "tap axis=Y");
+      break;
+    case ACCEL_AXIS_Z:
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "tap axis=Z");
+      if (direction == 1)
+      {
+        send_cmd();
+      }
+      break;
+  }
 }
 
 static void handle_bluetooth(bool connected) {
@@ -307,10 +339,10 @@ static void handle_time_tick(struct tm* tick_time, TimeUnits units_changed) {
     text_layer_set_text(date_layer, date_text);
     
   }
+  //Make sure that the weather is refreshed at least hourly
   if(units_changed & HOUR_UNIT) {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "Hour tick");
     send_cmd();
-    
   }
 
 }
@@ -323,6 +355,13 @@ static void init() {
   window_set_background_color(window, GColorBlack);
 
   Layer *window_layer = window_get_root_layer(window);
+
+  //PHONE COMM
+  comm_layer = bitmap_layer_create(GRect(35, 3, 10, 10));
+  layer_add_child(window_layer, bitmap_layer_get_layer(comm_layer));
+  comm_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMG_COMM_ON);
+  bitmap_layer_set_bitmap(comm_layer, comm_bitmap);
+  layer_mark_dirty(bitmap_layer_get_layer(comm_layer));
 
   //BLUETOOTH
   bt_layer = bitmap_layer_create(GRect(55, 3, 10, 10));
@@ -418,8 +457,8 @@ static void init() {
 
 
   //GET WEATHER FROM THE WEB
-  const int inbound_size = 64;
-  const int outbound_size = 64;
+  const int inbound_size = 128;
+  const int outbound_size = 128;
   app_message_open(inbound_size, outbound_size);
 
   Tuplet initial_values[] = {
@@ -431,8 +470,8 @@ static void init() {
   app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, 
       ARRAY_LENGTH(initial_values),
       sync_tuple_changed_callback, sync_error_callback, NULL);
-  //Make sure that the weather is refreshed at least hourly
-  handle_time_tick(current_time, HOUR_UNIT);
+  
+  // handle_time_tick(current_time, HOUR_UNIT);
 }
 
 static void deinit() {
@@ -446,6 +485,8 @@ static void deinit() {
   text_layer_destroy(date_layer);
   text_layer_destroy(temp_layer);
   text_layer_destroy(weather_loc_layer);
+  gbitmap_destroy(comm_bitmap);
+  bitmap_layer_destroy(comm_layer);
   gbitmap_destroy(bt_bitmap);
   bitmap_layer_destroy(bt_layer);
   gbitmap_destroy(battery_bitmap);
